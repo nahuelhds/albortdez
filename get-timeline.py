835da796375fd3c770438377661852725edda41e
Nahuel Sotelo <nahuel.sotelo@mercadolibre.com>
2020-04-14 11:28:30 -0300

    Get and save tweets from account

---
+++ get-timeline.py
@@ -0,0 +1,32 @@
+import sys
+import time
+import tweepy
+import configparser
+
+config = configparser.ConfigParser()
+config.read('.twitter')
+
+twitter_account = config['source']['account_screen_name']
+
+# OAuth process, using the keys and tokens
+auth = tweepy.OAuthHandler(config['consumer']['key'], config['consumer']['secret'])
+auth.set_access_token(config['access']['token'], config['access']['token_secret'])
+
+# Creation of the actual interface, using authentication
+api = tweepy.API(auth)
+
+saveFile = open('./tweets/%s.txt'%twitter_account, 'a')
+
+def limit_handled(cursor):
+    while True:
+        try:
+            yield cursor.next()
+        except tweepy.RateLimitError:
+            time.sleep(15 * 60)
+
+
+for status in limit_handled(tweepy.Cursor(api.user_timeline, screen_name=twitter_account, tweet_mode="extended").items()):
+    print(status.full_text)
+    saveFile.write("%s\n"%status.full_text)
+
+saveFile.close()
