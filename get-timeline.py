270887710c10950387e2bd528ff450213ddb9b16
Nahuel Sotelo <nahuel.sotelo@mercadolibre.com>
2020-04-14 23:41:44 -0300

    Get timeline minor mods

--- get-timeline.py
+++ get-timeline.py
@@ -1,21 +1,5 @@
-import sys
-import time
 import tweepy
 import configparser
-
-
-def limit_handled(cursor):
-    while True:
-        try:
-            yield cursor.next()
-        except tweepy.RateLimitError:
-            time.sleep(15 * 60)
-        except IOError:
-            time.sleep(60*5)
-            continue
-        except StopIteration:
-            break
-
 
 config = configparser.ConfigParser()
 config.read('.twitter')
@@ -26,12 +10,12 @@ auth = tweepy.OAuthHandler(config['consumer']['key'], config['consumer']['secret
 auth.set_access_token(config['access']['token'], config['access']['token_secret'])
 
 # Creation of the actual interface, using authentication
-api = tweepy.API(auth)
-saveFile = open('./tweets/%s.txt'%twitter_account, 'w')
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+saveFile = open('./tweets/%s.txt' % twitter_account, 'w')
 
-for status in limit_handled(tweepy.Cursor(api.user_timeline, screen_name=twitter_account, tweet_mode="extended").items()):
+for status in tweepy.Cursor(api.user_timeline, id=twitter_account, tweet_mode="extended").items():
     print(status.full_text)
-    saveFile.write("%s\n"%status.full_text)
+    saveFile.write("%s\n" % status.full_text)
 
 saveFile.close()
 
